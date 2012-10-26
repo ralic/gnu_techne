6f03eb9f71fb8f4638eccf01c42749e3d22dc2de
Dimitris Papavasiliou <dimitris@debian>
2012-10-26 18:04:08 +0300

    Stub for joint debug schematics.

--- src/joints/joints.lua
+++ src/joints/joints.lua
@@ -13,4 +13,57 @@
 -- You should have received a copy of the GNU General Public License    
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-return require 'joints.core'
+local core = require 'joints.core'
+
+if not options.drawjoints then
+   return core
+end
+
+local shading = require 'shading'
+local shapes = require 'shapes'
+
+return {
+   hinge = function (parameters)
+	    local hinge, oldmeta
+
+	    hinge = core.hinge (parameters)
+
+	    hinge.schematic = shading.flat {
+	       color = {0, 0, 1, 1},
+
+	       shaft = shapes.line {
+                  positions = {{0, 0, 0}},
+
+		  -- traverse = function (self) print (self) end,
+	       },
+	    }
+
+	    oldmeta = getmetatable(hinge)
+	    replacemetatable(hinge, {
+				__newindex = function (self, key, value)
+						if key == "axis" then
+						   self.schematic.shaft.positions =
+                                                      {{0, 0, 0},
+                                                       value}
+						end
+						
+						oldmeta.__newindex (self, key, value)
+					     end
+			     })
+
+	    return hinge
+	 end,
+
+  spherical = core.spherical,
+  angular = core.angular,
+  planar = core.planar,
+  clamp = core.clamp,
+  slider = core.slider,
+  contact = core.contact,
+  euler = core.euler,
+  gearing = core.gearing,
+  universal = core.universal,
+  doublehinge = core.doublehinge,
+  doubleball = core.doubleball,
+  linear = core.linear,
+}

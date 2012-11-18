854f641508d003490c597a7573e41ce5a6866307
Dimitris Papavasiliou <dpapavas@gmail.com>
2012-11-18 21:51:57 +0200

    Added a hinge joint schematic.

--- src/joints/joints.lua
+++ src/joints/joints.lua
@@ -25,6 +25,16 @@ local shading = require 'shading'
 local shapes = require 'shapes'
 local transform = require 'transform'
 local array = require 'array'
+local operations = require 'array.operations'
+local primitives = require 'primitives'
+
+local red = {1, 0, 0, 1}
+local white = {1, 1, 1, 1}
+local gold = {1, 0.69, 0, 1}
+local lightslategray = {0.1445, 0.2281, 0.3036}
+local steelblue = {0.0374, 0.2031, 0.4535}
+local lightsteelblue = {0.5812, 0.6223, 0.4535}
+local lightgoldenrod = {0.8441, 0.8667, 0.6463}
 
 return {
    hinge = function (parameters)
@@ -32,68 +42,138 @@ return {
 
 	    hinge = core.hinge (parameters)
 
-	    hinge.shaft = shading.flat {
-	       color = {0.45, 0.66, 0.86, 1},
+	    hinge.schematic = primitives.node {
+	       shaft = shading.flat {
+		  color = lightgoldenrod,
 
-	       prepare = function (self)
-		  local a, d, c, e
+		  prepare = function (self)
+		     local a, d, c, e
 
-		  d = self.bias or 0.1
-		  c = self.gain or 1 / 628
+		     d = self.bias or 0.1
+		     c = self.gain or 1 / 628
 
-		  a = hinge.anchor
-		  e = a + (d + c * hinge.state[2]) * hinge.axis
+		     a = hinge.anchor
+		     e = operations.add(a, operations.scale (hinge.axis, d + c * hinge.state[2]))
 
-		  self.lines.positions = array.doubles {a, e}
-		  self.points.positions = array.doubles {a, e}
-	       end,
+		     self.lines.positions = array.doubles {a, e}
+		     self.points.positions = array.doubles {a, e}
+		  end,
 
-	       lines = shapes.line {},
-	       points = shapes.points {},
-				       }
+		  lines = shapes.line {},
+		  points = shapes.points {},
+				    },
 
-	    hinge.arms = shading.flat {
-	       color = {0.3, 0.47, 0.62, 1},
+	       arms = shading.flat {
+		  color = lightsteelblue,
 
-	       prepare = function (self) 
-		  local a, b, pair
+		  prepare = function (self) 
+		     local a, b, pair
 
-		  pair = hinge.pair
-		  
-		  if pair then
-		     local positions = {}
+		     pair = hinge.pair
+		     
+		     if pair then
+			local positions = {}
 
-		     a = pair[1] and pair[1].position
-		     b = pair[2] and pair[2].position
+			a = pair[1] and pair[1].position
+			b = pair[2] and pair[2].position
 
-		     if a and b then
-			self.lines.positions = array.doubles{a,
-							     hinge.anchor,
-							     b}
+			if a and b then
+			   self.lines.positions = array.doubles{a,
+								hinge.anchor,
+								b}
 
-			self.points.positions = array.doubles{a, b}
-		     elseif a then
-			self.lines.positions = array.doubles{hinge.anchor, a}
-			self.points.positions = array.doubles{a}
-		     elseif b then
-			self.lines.positions = array.doubles{hinge.anchor, b}
-			self.points.positions = array.doubles{b}
+			   self.points.positions = array.doubles{a, b}
+			elseif a then
+			   self.lines.positions = array.doubles{hinge.anchor, a}
+			   self.points.positions = array.doubles{a}
+			elseif b then
+			   self.lines.positions = array.doubles{hinge.anchor, b}
+			   self.points.positions = array.doubles{b}
+			end
 		     end
-		  end
+		  end,
+
+		  lines = shapes.line {},
+		  points = shapes.line {},
+					 }
+					      }
+	    return hinge
+	 end,
+
+   slider = function (parameters)
+	    local slider, oldmeta, a, b
+
+	    slider = core.slider (parameters)
+
+	    slider.schematic = primitives.node {
+	       prepare = function (self)
+	    	  local a, b, L, l, h, x, d, stops, pair
+
+	    	  stops = slider.stops
+	    	  pair = slider.pair
+
+	    	  if not pair then
+	    	     return
+	    	  end
+
+	    	  a = pair[1] and pair[1].position
+	    	  b = pair[2] and pair[2].position
+	    	  x = slider.axis
+	    	  d = slider.state[1]
+
+	    	  if a and b then
+		     l, h = slider.stops[1][1], slider.stops[1][2]
+
+	    	     if l ~= -1 / 0 or h ~= 1 / 0 then
+			if math.abs(d - l) < 1e-3 * (h - l) then
+			   self.tube.color = red
+			else
+			   self.tube.color = gold
+			end
+
+			if math.abs(d - h) < 1e-3 * (h - l) then
+			   self.rod.color = red
+			else
+			   self.rod.color = white
+			end
+
+	    		d = d - l
+		     else
+			d = -0.5 * transform.dot (operations.subtract(b, a), x)
+	    	     end
+	    	  end
+		  
+		  self.tube.lines.positions = array.doubles {
+		     a, operations.add(b, operations.scale(x, d))
+							    }
+		  
+		  self.rod.lines.positions = array.doubles {
+		     b, operations.add(b, operations.scale(x, d))
+							   }
 	       end,
 
-	       lines = shapes.line {},
-	       points = shapes.line {},
-	    }
+	       rod = shading.flat {
+		  color = white,
 
-	    return hinge
+	    	  lines = shapes.line {},
+	    	  points = shapes.points {},
+	    			  },
+
+	       tube = shading.flat {
+		  color = gold,
+
+	    	  lines = shapes.line {},
+	    	  points = shapes.points {},
+	    			   }
+	    				       }
+
+	    return slider
 	 end,
 
   spherical = core.spherical,
   angular = core.angular,
   planar = core.planar,
   clamp = core.clamp,
-  slider = core.slider,
   contact = core.contact,
   euler = core.euler,
   gearing = core.gearing,

d5207e6bb1ddbcd116a74576bf9d7e67d5e9342f
Dimitris Papavasiliou <dpapavas@gmail.com>
2014-02-23 00:17:15 +0200

    Some corrections to the shading model for Grass.

--- src/glsl/grass_fragment.glsl.c
+++ src/glsl/grass_fragment.glsl.c
@@ -1,4 +1,4 @@
-in vec4 color_g;
+in vec4 color_g, position_g;
 in vec3 normal_g;
 in vec2 uv_g;
 flat in float distance_g;              
@@ -23,30 +23,28 @@ void main()
     vec4 texel;
 
     texel = texture(mask, uv_g);
-    
-    /* fragment = vec4(uv_g, 1, 1); return; */
-    /* fragment = texel.rgba; return; */
 
     if (distance_g < 0) {
-        fragment = color_g;
+        fragment = color_g * texel;
     } else if (texel.a > 0) {
-        vec3 L_e = direction;
+        vec3 l = direction;
         vec3 n = (gl_FrontFacing ? -1 : 1) * normalize(normal_g);
-        vec3 R = reflect(L_e, n);
-        float A, D_r, D_s, S, tau, nL_e;
+        vec3 v = normalize(-position_g.xyz);
+        float A, D, T, S, tau, nl;
 
-        nL_e = dot(n, L_e);
-
+        nl = dot(n, l);
         A = ambient[0] * mix(uv_g.y, 1, ambient[1]);
+        D = diffuse[0] * max(nl, 0);
 
-        tau = 1 / abs(nL_e);
-        D_r = diffuse[0] * max(nL_e, A);
-        D_s = diffuse[1] * max(-nL_e, A) * exp(-diffuse[2] * tau);
-
-        S = specular[0] * pow(max(-R.z, 0), specular[1]);
+        if (nl < 0) {
+            T = diffuse[0] * -nl * exp(diffuse[1] / nl);
+        } else {
+            T = 0;
+        }
+        
+        S = specular[0] * pow(max(dot(reflect(l, n), v), 0), specular[1]);
             
-        fragment = vec4(mix(texel.rgb + intensity * color_g.rgb * (D_r + mix(vec3(1), vec3(1, 1, 0.6), abs(nL_e)) * D_s), intensity, S), texel.a * color_g.a);
-        /* fragment = vec4(vec3(1, 0, 0) * D_s + vec3(0, 1, 0) * D_r, 1); */
+        fragment = vec4(texel.rgb + intensity * (color_g.rgb * (A + D + vec3(1, 1, 0.6) * T) + S), texel.a * color_g.a);
     } else {
         discard;
     }
--- src/glsl/grass_geometry.glsl.c
+++ src/glsl/grass_geometry.glsl.c
@@ -11,7 +11,7 @@ in vec3 position_te[], tangent_te[], bitangent_te[];
 in float distance_te[], height_te[], depth_te[], width_te[];
 in vec4 plane_te[], color_te[];
 
-out vec4 color_g;
+out vec4 color_g, position_g;
 out vec3 normal_g;
 out vec2 uv_g;
 flat out float distance_g;              
@@ -23,7 +23,7 @@ void main()
     const vec3 l = direction_w;
     const vec2 horizon = vec2(100, 1.0 / 3.0);
 
-    mat4 T;
+    mat4 M, P;
     mat3 R;
     vec4 h, p_0, p_1, p;
     float z, z_f, n;
@@ -32,28 +32,28 @@ void main()
     n = exp(3e-4 * depth_te[0] * depth_te[0] * (1 / (1 + exp(z_f))));    
     h = 0.5 * n * width_te[0] * vec4(bitangent_te[0], 0);
 
+    P = projection;
+    M = modelview;
+    R = mat3(modelview);
+    
 #ifdef CAST_SHADOWS
     if (gl_InvocationID == 0) {
 #endif
         color_g = color_te[0];    
         distance_g = distance_te[0];
 
-        T = transform;
 #ifdef CAST_SHADOWS
     } else {
         mat4 P;
         vec4 L;
 
         L = vec4(l / dot(l, plane_te[0].xyz), 0);
-        P = mat4(1.0) - outerProduct(L, plane_te[0]);
-        T = transform * P;
+        M *= mat4(1.0) - outerProduct(L, plane_te[0]);
         
         color_g = vec4(0, 0, 0, 0.6);
         distance_g = -1;
     }
 #endif
-
-    R = mat3(modelview);
     
     p_0 = vec4(position_te[0], 1);
     p_1 = vec4(position_te[1], 1);    
@@ -61,21 +61,25 @@ void main()
     normal_g = R * tangent_te[0];
     
     uv_g = vec2(1, height_te[0]);
-    gl_Position = T * (p_0 + h);
+    position_g = M * (p_0 + h);
+    gl_Position = P * position_g;
     EmitVertex();
 
     uv_g = vec2(0, height_te[0]);
-    gl_Position = T * (p_0 - h);
+    position_g = M * (p_0 - h);
+    gl_Position = P * position_g;
     EmitVertex();
 
     normal_g = R * tangent_te[1];
     
     uv_g = vec2(1, height_te[1]);
-    gl_Position = T * (p_1 + h);
+    position_g = M * (p_1 + h);
+    gl_Position = P * position_g;
     EmitVertex();
 
     uv_g = vec2(0, height_te[1]);
-    gl_Position = T * (p_1 - h);
+    position_g = M * (p_1 - h);
+    gl_Position = P * position_g;
     EmitVertex();
 
     gl_PrimitiveID = gl_PrimitiveIDIn;
